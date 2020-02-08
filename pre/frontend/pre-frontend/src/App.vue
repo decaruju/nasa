307366a17f3c8835f697328805f6d764535b3cd0
Julien de Carufel <julien.decarufel@gmail.com>
2020-02-08 13:06:11 -0500

    push all the things

--- pre/frontend/pre-frontend/src/App.vue
+++ pre/frontend/pre-frontend/src/App.vue
@@ -1,22 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    instacript
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
+  html, body, .app-body {
+  height: 100%;
+  }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
